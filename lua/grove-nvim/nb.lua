562fad72c54546c7fd158b3c904c9e5e2144a228
Matthew Solomonson <ggmatt@gmail.com>
2025-10-08 09:10:43 -0400

    fix: exclude plan types in nb manage

--- lua/grove-nvim/nb.lua
+++ lua/grove-nvim/nb.lua
@@ -2,11 +2,11 @@
 -- Grove Notebook (nb) integration
 
 local M = {}
-local utils = require('grove-nvim.utils')
+local utils = require("grove-nvim.utils")
 
 -- Open nb manage TUI
 function M.manage()
-  utils.run_in_float_term_tui('nb manage', 'NB Manage')
+	utils.run_in_float_term_tui("nb manage --exclude-type plans", "NB Manage")
 end
 
 return M
